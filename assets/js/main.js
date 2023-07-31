16ba0ac634ed36d7eeae182a9a9c5473e79414e2
Jahid Munshi <jahidmunshi99@gmail.com>
2023-07-31 23:11:56 +0600

    responsive menubar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 // Toggle For Navbar
-const toggleIcon = document.querySelector('.fa-bars');
-const mainMenu = document.querySelector('.navbar-nav');
+// const toggleIcon = document.querySelector('.fa-bars');
+// const mainMenu = document.querySelector('.navbar-nav');
 
-toggleIcon.addEventListener('click', ()=>{
+// toggleIcon.addEventListener('click', ()=>{
+//     mainMenu.classList.toggle("active");
+//     console.log(alert('Hi I"m Here'))
+// })
+
+// Toggle Button for Main Menu
+const toggleButton = document.querySelector("#toggle-icon");
+const mainMenu = document.querySelector("#main-menu");
+
+toggleButton.addEventListener("click",()=>{
     mainMenu.classList.toggle("active");
-    // console.log(alert('Hi I"m Here'))
+    console.log('Hi This Is Jahid')
 })
